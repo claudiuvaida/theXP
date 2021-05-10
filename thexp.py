606669d28e701f716b29979bd6e34a87870e7bec
claudiuvaida <claudiu.vaida@gmail.com>
2021-05-10 21:38:49 +0300

    Adding the second driver for execution

--- thexp.py
+++ thexp.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 import time
 
 number = input ("How many times?\n")
@@ -8,13 +10,26 @@ contract = input ("Contract address?\n")
 i = 1
 
 while i <= int(number):
+    #Defining and installing drivers using webdriver manager dependency
     driver=webdriver.Chrome(ChromeDriverManager().install())
-    driver.minimize_window()
+    driver2=webdriver.Edge(EdgeChromiumDriverManager().install())
+    
+    #Opening dextools using the defined drivers
     driver.get("https://www.dextools.io/app/pancakeswap/pair-explorer/" + contract)
-    print("Application url is", driver.current_url)
-    time.sleep(10)
+    driver2.get("https://www.dextools.io/app/pancakeswap/pair-explorer/" + contract)
+
+    ##Minimize windows for both drivers
+    driver.minimize_window()
+    driver2.minimize_window()
+
+    #Wait 7 seconds
+    time.sleep(7)
+
+    #Quit/Close driver
     driver.quit()
-    print(i)
+    driver2.quit()
+
+    print("Execution ID: " + i)
     i += 1
 
-print("End")
+print("-- End --")
