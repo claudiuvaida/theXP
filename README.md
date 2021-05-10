76a3ce454937c8eef870597a448778e7e498bf39
claudiuvaida <claudiu.vaida@gmail.com>
2021-05-10 20:43:43 +0300

    Adding input

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 
 Install:
 
-- Python (any version)
+- Python
 - Visual Studio Code
 
 After installing the required tools, open the project folder from visual studio code and the open a new terminal from visual studio code in order to execute the following commands:
 
+``
 pip install selenium
-
 pip install webdriver-manager
+``
+
+Usage:
+
+Open a terminal (and execute it via cmd) or click on thexp.py and provide the number of times for script execution and the contract address.
--- thexp.py
+++ thexp.py
@@ -2,16 +2,17 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+number = input ("How many times?\n")
+contract = input ("Contract address?\n")
+
 i = 1
 
-while i < 6:
-    contract = "0x1c12a5538173f4ef4f8d965e6189ee60d7f93a4f"
+while i <= int(number):
     driver=webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     driver.get("https://www.dextools.io/app/pancakeswap/pair-explorer/" + contract)
-    print("Application title is ", driver.title)
     print("Application url is", driver.current_url)
-    time.sleep(3)
+    time.sleep(10)
     driver.quit()
     print(i)
     i += 1
